016d3572559f58ca9ad8d1b96e46d6016005cd35
Asnain <asnain.khan@outlook.com>
2025-02-07 15:14:58 +0000

    Added @AfterEach tearDown()

--- src/test/java/org/mercatordigital/technicaltask/hooks/WebHooks.java
+++ src/test/java/org/mercatordigital/technicaltask/hooks/WebHooks.java
@@ -1,25 +1,39 @@
 package org.mercatordigital.technicaltask.hooks;
 
 import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 public class WebHooks {
 
     private static BrowserContext context;
+    private static Browser browser;
     public Page page;
 
     @BeforeAll
     public static void initialisePlaywright() {
 
         Playwright playwright = Playwright.create();
-        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        context = browser.newContext();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @BeforeEach
     public void createPage() {
 
+        context = browser.newContext();
         page = context.newPage();
+    }
+
+    @AfterEach
+    public void tearDown() {
+
+        if (context != null) {
+            context.close();
+        }
+
+        if (page != null) {
+            page.close();
+        }
     }
 }
